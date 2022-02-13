fb885826d33bf8a0a8b0489e89d736e580a036e9
kohei-san <kohei.miyatake@gmail.com>
2022-02-13 12:13:17 +0900

    Initial Commit

---
+++ targetNoteTrainer/targetNoteTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  targetNoteTrainerApp.swift
+//  targetNoteTrainer
+//
+//  Created by 宮武昂平 on 2022/02/13.
+//
+
+import SwiftUI
+
+@main
+struct targetNoteTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
